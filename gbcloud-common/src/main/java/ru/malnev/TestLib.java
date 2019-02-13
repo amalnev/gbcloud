e04d9a462161a3ce324df5d062dc5207dca2db2a
a.malnev <a.malnev@gmail.com>
2019-02-13 11:32:23 +0300

    Создан скелет maven-проекта для облачного хранилища с модулями client, server и common

---
+++ gbcloud-common/src/main/java/ru/malnev/TestLib.java
@@ -0,0 +1,14 @@
+package ru.malnev;
+
+public class TestLib
+{
+    int f()
+    {
+       return 10;
+    }
+
+    int g()
+    {
+        return 20;
+    }
+}
---
+++ gbcloud-server/src/main/java/ru/malnev/App.java
@@ -0,0 +1,14 @@
+package ru.malnev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        TestLib lib = new TestLib();
+        System.out.println( lib.g() );
+    }
+}
